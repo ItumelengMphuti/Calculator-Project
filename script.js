23109778ffa9a02d1154c10696330fe065193391
ItumelengMphuti <125954886+ItumelengMphuti@users.noreply.github.com>
2024-03-06 22:49:49 +0200

    New Updates

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const calculate = (n1, operator, n2) => {
   
   keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
-      const key = e.target;
+      const key  = e.target;
       const action = key.dataset.action;
       const keyContent = key.textContent;
       const displayedNum = display.textContent;
